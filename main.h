1e7a0586dba22f9f1c09316360b25a90a2ee5108
kwenamabotja <koena93@gmail.com>
2022-10-18 20:50:27 +0200

    task3

--- main.h
+++ main.h
@@ -10,9 +10,15 @@ int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_string(va_list s);
 int  (*get_func(const char *format))(va_list);
-int print_binary(va_list b);
+
 int print_int(va_list i);
 int print_dec(va_list d);
+
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
 
 /**
   *struct special_chr - all special charectors
