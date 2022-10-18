dc7898d9df4054a3c590f12cf6a7dbb286b39753
kwenamabotja <koena93@gmail.com>
2022-10-18 17:11:48 +0200

    fix for task 0

--- main.h
+++ main.h
@@ -1,28 +1,30 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTONH
+#define HOLBERTONH
 
-#include <stdlib.h>
 #include <stdarg.h>
-
 int _putchar(char c);
-int _puts(char *str);
 int _printf(const char *format, ...);
 int print_char(va_list c);
 int print_string(va_list s);
-int  (*get_func(const char *format))(va_list);
-
 int print_int(va_list i);
 int print_dec(va_list d);
-
+int print_rev(va_list r);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_rot13(va_list R);
 /**
-  *struct special_chr - all special charectors
-  *@chr: special charector
-  *@f: function
+  * struct code_format - Struct format
+  *
+  * @sc: The specifiers
+  * @f: The function associated
   */
-typedef struct special_chr
+typedef struct code_format
 {
-	char *chr;
+	char *sc;
 	int (*f)(va_list);
-} special_chr;
+} code_f;
 
-#endif
+#endif /* HOLBERTONH */
