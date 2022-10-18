8961b1c61136b2ccbf3c30869c4b186547096d17
Edward Ndlala <Edwardthina@gmail.com>
2022-10-18 10:35:34 +0000

    main

--- main.h
+++ main.h
@@ -8,6 +8,7 @@ int _putchar(char c);
 int _puts(char *str);
 int _printf(const char *format, ...);
 int  (*get_func(const char *format))(va_list);
+int print_string(va_list s);
 /**
   *struct  Special_chr - all special charectors
   *@chr: special charector
