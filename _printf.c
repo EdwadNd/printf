22f0bc5e33844654db9611d8ed21955f9faa98ca
kwenamabotja <koena93@gmail.com>
2022-10-18 12:42:26 +0200

    printf

--- _printf.c
+++ _printf.c
@@ -1,8 +1,6 @@
-  #include <stdio.h>
-   #include <stdarg.h>
-   #include <unistd.h>
-   #include "main.h"
-  
+#include <stdarg.h>
+#include <unistd.h>
+#include "main.h"
 /**
   * find_function - function that finds formats for _printf
   * calls the corresponding function.
@@ -35,47 +33,46 @@ int (*find_function(const char *format))(va_list)
 	}
 	return (NULL);
 }
+/**
+  * _printf - function that produces output according to a format.
+  * @format: format (char, string, int, decimal)
+  * Return: size the output text;
+  */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int (*f)(va_list);
+	unsigned int i = 0, cprint = 0;
 
-  /**
-     *_printf- prints out a string based on the formet given
-     *@format: formet
-     *Return: the number of characters printed
-     */
- int _printf(const char *format, ...)
-  {
-          va_list ap;
-          int (*f)(va_list);
-          unsigned int i = 0, cprint = 0;
- 
-          if (format == NULL)
-                  return (-1);
-          va_start(ap, format);
-          while (format[i])
-          {
-                  while (format[i] != '%' && format[i])
-                  {
-                          _putchar(format[i]);
-                          cprint++;
-                          i++;
-                 }
-                  if (format[i] == '\0')
-                          return (cprint);
-                  f = get_func(&format[i + 1]);
-                  if (f != NULL)
-                  {
-                          cprint += f(ap);
-                          i += 2;
-                          continue;
-                  }
-                  if (!format[i + 1])
-                          return (-1);
-                  _putchar(format[i]);
-                  cprint++;
-                  if (format[i + 1] == '%')
-                          i += 2;
-                  else
-                          i++;
-          }
-          va_end(ap);
-          return (cprint);
- }
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	while (format[i])
+	{
+		while (format[i] != '%' && format[i])
+		{
+			_putchar(format[i]);
+			cprint++;
+			i++;
+		}
+		if (format[i] == '\0')
+			return (cprint);
+		f = find_function(&format[i + 1]);
+		if (f != NULL)
+		{
+			cprint += f(ap);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		cprint++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
+			i++;
+	}
+	va_end(ap);
+	return (cprint);
+}
