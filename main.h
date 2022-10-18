c7e7e46769d5ca21700c6344f78e093ad9cf0533
Edward Ndlala <Edwardthina@gmail.com>
2022-10-18 04:56:25 +0000

    main.h struct

--- main.h
+++ main.h
@@ -8,4 +8,15 @@ int _putchar(char c);
 int _puts(char *str);
 int _printf(const char *format, ...);
 
+/**
+  *struct  Special_chr - all special charectors
+  *@chr: special charector
+  *@f: function
+  */
+typedef struct special_chr
+{
+	char *chr;
+	int (*f)(va_list);
+}special_chr;
+
 #endif
